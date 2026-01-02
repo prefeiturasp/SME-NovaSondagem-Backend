d822a399917c9afe104f3be117aa306c544bb8ef
wesleygyn <wesley_gyn@yahoo.com.br>
2026-01-02 14:17:29 -0300

    Ajuste sonar  classe "RegistraAutenticacao"

--- src/SME.Sondagem.API/Configuracoes/RegistraAutenticacao.cs
+++ src/SME.Sondagem.API/Configuracoes/RegistraAutenticacao.cs
@@ -15,24 +15,29 @@ public static class RegistraAutenticacao
             o.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
             o.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
         })
-           .AddJwtBearer(AutenticacaoSettingsApi.BearerTokenSGP, o =>
-           {
-               o.TokenValidationParameters = new TokenValidationParameters
-               {
-                   ValidateLifetime = true,
-                   ValidateAudience = true,
-                   ValidAudience = configuration.GetValue<string>("SGPApiTokenSettings:Audience"),
-                   ValidateIssuer = true,
-                   ValidIssuer = configuration.GetValue<string>("SGPApiTokenSettings:Issuer"),
-                   ValidateIssuerSigningKey = true,
-                   ClockSkew = TimeSpan.Zero,
-                   IssuerSigningKey = new SymmetricSecurityKey(UTF8
-                       .GetBytes(configuration.GetValue<string>("SGPApiTokenSettings:IssuerSigningKey")))
-               };
-               o.MapInboundClaims = false;
-           })
-       .AddJwtBearer(AutenticacaoSettingsApi.BearerTokenSondagem, options =>
+        .AddJwtBearer(AutenticacaoSettingsApi.BearerTokenSGP, o =>
         {
+            var issuerSigningKey = configuration.GetValue<string>("SGPApiTokenSettings:IssuerSigningKey")
+                ?? throw new InvalidOperationException("SGPApiTokenSettings:IssuerSigningKey não configurado");
+
+            o.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateLifetime = true,
+                ValidateAudience = true,
+                ValidAudience = configuration.GetValue<string>("SGPApiTokenSettings:Audience"),
+                ValidateIssuer = true,
+                ValidIssuer = configuration.GetValue<string>("SGPApiTokenSettings:Issuer"),
+                ValidateIssuerSigningKey = true,
+                ClockSkew = TimeSpan.Zero,
+                IssuerSigningKey = new SymmetricSecurityKey(UTF8.GetBytes(issuerSigningKey))
+            };
+            o.MapInboundClaims = false;
+        })
+        .AddJwtBearer(AutenticacaoSettingsApi.BearerTokenSondagem, options =>
+        {
+            var issuerSigningKey = configuration.GetValue<string>("SondagemTokenSettings:IssuerSigningKey")
+                ?? throw new InvalidOperationException("SondagemTokenSettings:IssuerSigningKey não configurado");
+
             options.TokenValidationParameters = new TokenValidationParameters
             {
                 ValidateIssuer = true,
@@ -41,18 +46,16 @@ public static class RegistraAutenticacao
                 ValidateIssuerSigningKey = true,
                 ValidIssuer = configuration.GetValue<string>("SondagemTokenSettings:Issuer"),
                 ValidAudience = configuration.GetValue<string>("SondagemTokenSettings:Audience"),
-                IssuerSigningKey = new SymmetricSecurityKey(UTF8.GetBytes(configuration.GetValue<string>("SondagemTokenSettings:IssuerSigningKey")))
+                IssuerSigningKey = new SymmetricSecurityKey(UTF8.GetBytes(issuerSigningKey))
             };
         });
-;
 
         services.AddAuthorization(auth =>
         {
             auth.AddPolicy("Bearer", new AuthorizationPolicyBuilder()
-
-               .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme)
-               .RequireAuthenticatedUser()
-               .Build());
+                .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme)
+                .RequireAuthenticatedUser()
+                .Build());
         });
     }
 }
