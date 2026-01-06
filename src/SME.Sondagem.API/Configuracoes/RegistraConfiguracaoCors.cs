c90ab80f5f1b2c4e40778c8106bc3906c0ae326c
Mario Junior <mariosj9@gmail.com>
2026-01-06 17:34:36 -0300

    hotfix RegistraConfiguracaoCors

--- src/SME.Sondagem.API/Configuracoes/RegistraConfiguracaoCors.cs
+++ src/SME.Sondagem.API/Configuracoes/RegistraConfiguracaoCors.cs
@@ -6,39 +6,27 @@
         {
             builder.Services.AddCors(options =>
             {
-                if (builder.Environment.IsDevelopment())
+                var allowedOriginsString = builder.Configuration["Cors:AllowedOrigins"];
+                var allowedOrigins = string.IsNullOrWhiteSpace(allowedOriginsString)
+                    ? []
+                    : allowedOriginsString.Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+
+                options.AddPolicy("CorsPolicy", policy =>
                 {
-                    options.AddPolicy("CorsPolicy", policy =>
+                    if (allowedOrigins.Length > 0)
                     {
-                        policy.AllowAnyOrigin()
+                        policy.WithOrigins(allowedOrigins)
+                              .AllowAnyMethod()
+                              .AllowAnyHeader()
+                              .AllowCredentials();
+                    }
+                    else
+                    {
+                        policy.WithOrigins("https://localhost")
                               .AllowAnyMethod()
                               .AllowAnyHeader();
-                    });
-                }
-                else
-                {
-                    var allowedOriginsString = builder.Configuration["Cors:AllowedOrigins"];
-                    var allowedOrigins = string.IsNullOrWhiteSpace(allowedOriginsString)
-                        ? []
-                        : allowedOriginsString.Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
-
-                    options.AddPolicy("CorsPolicy", policy =>
-                    {
-                        if (allowedOrigins.Length > 0)
-                        {
-                            policy.WithOrigins(allowedOrigins)
-                                  .AllowAnyMethod()
-                                  .AllowAnyHeader()
-                                  .AllowCredentials();
-                        }
-                        else
-                        {
-                            policy.WithOrigins("https://localhost")
-                                  .AllowAnyMethod()
-                                  .AllowAnyHeader();
-                        }
-                    });
-                }
+                    }
+                });
             });
         }
     }
