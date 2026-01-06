aa9f355987cc1185f5fdfa060e7e47cecfe24633
Mario Junior <mariosj9@gmail.com>
2026-01-06 17:39:29 -0300

    ajuste code smell medium

--- src/SME.Sondagem.Dominio/Constantes/MensagensNegocio/MensagemNegocioComuns.cs
+++ src/SME.Sondagem.Dominio/Constantes/MensagensNegocio/MensagemNegocioComuns.cs
@@ -3,7 +3,7 @@
 namespace SME.Sondagem.Dominio.Constantes.MensagensNegocio;
 
 [ExcludeFromCodeCoverage]
-public class MensagemNegocioComuns
+public static class MensagemNegocioComuns
 {
     public const string REQUISICAO_CANCELADA = "Requisição cancelada pelo cliente"; 
     public const string CICLO_NAO_ENCONTRADO = "Ciclo com ID {0} não encontrado";
