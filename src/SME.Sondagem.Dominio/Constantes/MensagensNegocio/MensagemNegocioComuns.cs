31bd79e33f089c9139d1789102a3472a821335b0
Thiago Fernando <thiago.fernando@msn.com>
2026-01-12 14:17:48 -0300

    Resolvido conflito mensagem negocio

--- src/SME.Sondagem.Dominio/Constantes/MensagensNegocio/MensagemNegocioComuns.cs
+++ src/SME.Sondagem.Dominio/Constantes/MensagensNegocio/MensagemNegocioComuns.cs
@@ -11,8 +11,8 @@ public static class MensagemNegocioComuns
     public const string COMPONENTE_CURRICULAR_EOL_NAO_ENCONTRADO = "Componente curricular com código EOL {0} não encontrado";
     public const string PROEFICIENCIA_NAO_ENCONTRADA = "Proficiência com ID {0} não encontrada";
     public const string QUESTAO_NAO_ENCONTRADA = "Questão com ID {0} não encontrada";
-    public const string NENHUM_REGISTRO_ENCONTRADO = "Nenhum registro encontrado";
-    public const string INFORMAR_ID_MAIOR_QUE_ZERO = "Informe um valor maior que zero";
     public const string NENHUM_SONDAGEM_ATIVA_ENCONRADA = "Nenhuma sondagem ativa encontrada para salvar as respostas.";
     public const string SALVAR_SOMENTE_PARA_SONDAGEM_ATIVA = "As respostas só podem ser salvas para a sondagem ativa.";
+    public const string NENHUM_REGISTRO_ENCONTRADO = "Nenhum registro encontrado";
+    public const string INFORMAR_ID_MAIOR_QUE_ZERO = "Informe um valor maior que zero";
 }
