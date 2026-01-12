7ec84122f8ee83b84248144c2bfa0e989b81be99
Thiago Fernando <thiago.fernando@msn.com>
2026-01-12 17:08:54 -0300

    Correção de code smell

--- src/SME.Sondagem.Aplicacao/UseCases/Sondagem/SondagemSalvarRespostasUseCase.cs
+++ src/SME.Sondagem.Aplicacao/UseCases/Sondagem/SondagemSalvarRespostasUseCase.cs
@@ -11,6 +11,7 @@ namespace SME.Sondagem.Aplicacao.UseCases.Sondagem
     {
         private readonly IRepositorioSondagem _repositorioSondagem;
         private readonly IRepositorioRespostaAluno _repositorioSondagemResposta;
+
         public SondagemSalvarRespostasUseCase(IRepositorioSondagem repositorioSondagem, IRepositorioRespostaAluno repositorioSondagemResposta)
         {
             _repositorioSondagem = repositorioSondagem;
@@ -19,58 +20,103 @@ namespace SME.Sondagem.Aplicacao.UseCases.Sondagem
 
         public async Task<bool> SalvarOuAtualizarSondagemAsync(SondagemSalvarDto dto)
         {
-            var sondagemAtiva = await _repositorioSondagem.ObterSondagemAtiva();
-            var respostas = new List<RespostaAluno>();
-            var sondagemId = dto.SondagemId;
-            
-            if (sondagemAtiva == null)
-               throw new NegocioException(MensagemNegocioComuns.NENHUM_SONDAGEM_ATIVA_ENCONRADA);
-
-            if (sondagemAtiva.Id != dto.SondagemId)
-                throw new NegocioException(MensagemNegocioComuns.SALVAR_SOMENTE_PARA_SONDAGEM_ATIVA);
-
+            var sondagemAtiva = await ObterEValidarSondagemAtiva(dto.SondagemId);
             var periodosBimestresAtivos = sondagemAtiva.PeriodosBimestre.Where(x => !x.Excluido);
 
             var alunosIds = dto.Alunos.Select(a => a.AlunoId);
             var questoresIds = dto.Alunos.SelectMany(a => a.Respostas.Select(r => r.QuestaoId));
-            var repostasAlunos = await _repositorioSondagemResposta.ObterRespostasPorSondagemEAlunosAsync(sondagemId, alunosIds, questoresIds);
+            var repostasAlunos = await _repositorioSondagemResposta.ObterRespostasPorSondagemEAlunosAsync(dto.SondagemId, alunosIds, questoresIds);
+
+            var respostas = ProcessarRespostasAlunos(dto, periodosBimestresAtivos, repostasAlunos);
+
+            return await _repositorioSondagemResposta.SalvarAsync(respostas);
+        }
+
+        private async Task<Dominio.Entidades.Sondagem.Sondagem> ObterEValidarSondagemAtiva(int sondagemId)
+        {
+            var sondagemAtiva = await _repositorioSondagem.ObterSondagemAtiva();
+
+            if (sondagemAtiva == null)
+                throw new NegocioException(MensagemNegocioComuns.NENHUM_SONDAGEM_ATIVA_ENCONRADA);
+
+            return sondagemAtiva.Id != sondagemId ? throw new NegocioException(MensagemNegocioComuns.SALVAR_SOMENTE_PARA_SONDAGEM_ATIVA) : sondagemAtiva;
+        }
+
+        private List<RespostaAluno> ProcessarRespostasAlunos(
+            SondagemSalvarDto dto,
+            IEnumerable<SondagemPeriodoBimestre> periodosBimestresAtivos,
+            IEnumerable<RespostaAluno> repostasAlunos)
+        {
+            var respostas = new List<RespostaAluno>();
 
             foreach (var aluno in dto.Alunos)
             {
                 foreach (var respostaDto in aluno.Respostas)
                 {
-                   var periodoBimestreAtivo = periodosBimestresAtivos
-                        .FirstOrDefault(pb => pb.BimestreId == respostaDto.BimestreId);
+                    var resposta = ProcessarRespostaIndividual(
+                        dto.SondagemId,
+                        aluno.AlunoId,
+                        respostaDto,
+                        periodosBimestresAtivos,
+                        repostasAlunos);
 
-                    if (periodoBimestreAtivo == null)
-                        continue;
-
-                    if (periodoBimestreAtivo.DataInicio < DateTime.Now || periodoBimestreAtivo.DataFim > DateTime.Now)
-                        continue;
-
-                    if(repostasAlunos is not null )
-                    {
-                        var respostaExistente = repostasAlunos
-                            .FirstOrDefault(r => r.AlunoId == aluno.AlunoId && r.QuestaoId == respostaDto.QuestaoId);
-                        if (respostaExistente == null) continue;
-                        respostaExistente.AtualizarResposta(respostaDto.OpcaoRepostaId, DateTime.UtcNow);
-                        respostas.Add(respostaExistente);
-                    }
-                    else
-                    {
-                        respostas.Add(new RespostaAluno(
-                            sondagemId,
-                            aluno.AlunoId,
-                            respostaDto.QuestaoId,
-                            respostaDto.OpcaoRepostaId,
-                            DateTime.UtcNow,
-                            respostaDto.BimestreId
-                        ));
-                    }
+                    if (resposta != null)
+                        respostas.Add(resposta);
                 }
             }
 
-            return await _repositorioSondagemResposta.SalvarAsync(respostas);
+            return respostas;
+        }
+
+        private RespostaAluno? ProcessarRespostaIndividual(
+            int sondagemId,
+            int alunoId,
+            RespostaSondagemDto respostaDto,
+            IEnumerable<SondagemPeriodoBimestre> periodosBimestresAtivos,
+            IEnumerable<RespostaAluno> repostasAlunos)
+        {
+            var periodoBimestreAtivo = periodosBimestresAtivos
+                .FirstOrDefault(pb => pb.BimestreId == respostaDto.BimestreId);
+
+            if (!ValidarPeriodoBimestre(periodoBimestreAtivo))
+                return null;
+
+            var respostaExistente = repostasAlunos?.FirstOrDefault(r => 
+                r.AlunoId == alunoId && r.QuestaoId == respostaDto.QuestaoId);
+
+            return CriarOuAtualizarResposta(
+                sondagemId,
+                alunoId,
+                respostaDto,
+                respostaExistente);
+        }
+
+        private bool ValidarPeriodoBimestre(SondagemPeriodoBimestre periodoBimestreAtivo)
+        {
+            if (periodoBimestreAtivo == null)
+                return false;
+
+            return periodoBimestreAtivo.DataInicio >= DateTime.Now && 
+                   periodoBimestreAtivo.DataFim <= DateTime.Now;
+        }
+
+        private RespostaAluno CriarOuAtualizarResposta(
+            int sondagemId,
+            int alunoId,
+            RespostaSondagemDto respostaDto,
+            RespostaAluno respostaExistente)
+        {
+            if (respostaExistente == null)
+                return new RespostaAluno(
+                    sondagemId,
+                    alunoId,
+                    respostaDto.QuestaoId,
+                    respostaDto.OpcaoRepostaId,
+                    DateTime.UtcNow,
+                    respostaDto.BimestreId);
+            respostaExistente.AtualizarResposta(respostaDto.OpcaoRepostaId, DateTime.UtcNow);
+            return respostaExistente;
+
         }
     }
 }
