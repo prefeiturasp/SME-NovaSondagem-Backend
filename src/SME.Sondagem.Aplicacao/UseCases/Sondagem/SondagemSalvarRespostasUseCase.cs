44528d2b334d347042cd7cf630c64b0bedf5aabe
Flavio-Spassu <flavio.souza@spassu.com.br>
2026-01-12 10:06:44 -0300

    adicionado validacoes sondagem ativa

--- src/SME.Sondagem.Aplicacao/UseCases/Sondagem/SondagemSalvarRespostasUseCase.cs
+++ src/SME.Sondagem.Aplicacao/UseCases/Sondagem/SondagemSalvarRespostasUseCase.cs
@@ -1,6 +1,7 @@
 ﻿using SME.Sondagem.Aplicacao.Interfaces.Sondagem;
 using SME.Sondagem.Dados.Interfaces;
 using SME.Sondagem.Dominio.Entidades.Sondagem;
+using SME.Sondagem.Infra.Exceptions;
 using SME.Sondagem.Infrastructure.Dtos.Sondagem;
 
 namespace SME.Sondagem.Aplicacao.UseCases.Sondagem
@@ -17,8 +18,20 @@ namespace SME.Sondagem.Aplicacao.UseCases.Sondagem
 
         public async Task<bool> SalvarOuAtualizarSondagemAsync(SondagemSalvarDto dto)
         {
+            var sondagemAtiva = await _repositorioSondagem.ObterSondagemAtiva();
             var respostas = new List<RespostaAluno>();
             var sondagemId = dto.SondagemId;
+
+            //sondagemAtiva.PeriodosBimestre
+            // comparar com o que vem dentro do aluno.respostas
+            
+            if (sondagemAtiva == null)
+               throw new NegocioException("Nenhuma sondagem ativa encontrada para salvar as respostas.");
+
+            if (sondagemAtiva.Id != dto.SondagemId)
+                throw new NegocioException("As respostas só podem ser salvas para a sondagem ativa.");
+
+            // criar uma régra que valida se o bimestre enviado pelo front está ativo na sondagem.periodosBimestres
 
             foreach (var aluno in dto.Alunos)
             {
--- src/SME.Sondagem.Infra/Dtos/Sondagem/RespostaSondagemDto.cs
+++ src/SME.Sondagem.Infra/Dtos/Sondagem/RespostaSondagemDto.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace SME.Sondagem.Infrastructure.Dtos.Sondagem
+﻿namespace SME.Sondagem.Infrastructure.Dtos.Sondagem
 {
     public class RespostaSondagemDto
     {
+        //bimestreId
+        //questaoId
         public string NomeColuna { get; set; } = null!;  // 1° ciclo, 2° ciclo...
         public int RespostaId { get; set; }     // questao_id
         public int RespostaSelecionada { get; set; }  // opcao_resposta_id
