92e6a1679439e7b6bc7dfc5885b1d47117c61ed4
Flavio-Spassu <flavio.souza@spassu.com.br>
2026-01-19 13:44:33 -0300

    Alterado nome componente curricular português

---
+++ src/SME.Sondagem.Dados/Migrations/20260119163321_AlterComponenteCurricularPortugues.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SME.Sondagem.Dados.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlterComponenteCurricularPortugues : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                "UPDATE componente_curricular SET nome = 'Língua Portuguesa' WHERE nome = 'Português'");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
