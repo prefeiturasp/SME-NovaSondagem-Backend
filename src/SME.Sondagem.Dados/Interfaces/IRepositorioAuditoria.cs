19ff090f8300e47130ad05feac51ec7d22f8526d
Thiago Fernando <thiago.fernando@msn.com>
2026-01-20 14:15:07 -0300

    Ajustes code smell

--- src/SME.Sondagem.Dados/Interfaces/IRepositorioAuditoria.cs
+++ src/SME.Sondagem.Dados/Interfaces/IRepositorioAuditoria.cs
@@ -1,10 +1,7 @@
 using SME.Sondagem.Dados.Interfaces.Elastic;
-using SME.Sondagem.Dominio.Entidades;
-using System.Diagnostics.CodeAnalysis;
 
 namespace SME.Sondagem.Dados.Interfaces;
 
-[ExcludeFromCodeCoverage]
 public interface IRepositorioAuditoria : IRepositorioElasticBase<Dominio.Entidades.Auditoria>
 {
     Task Salvar(Dominio.Entidades.Auditoria auditoria);
