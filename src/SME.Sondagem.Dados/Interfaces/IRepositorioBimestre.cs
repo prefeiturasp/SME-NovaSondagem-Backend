e9a12154e4bb16da7a1ce17220846fbdfc81c56f
Thiago Fernando <thiago.fernando@msn.com>
2026-01-29 15:18:30 -0300

    Remover comentarios

--- src/SME.Sondagem.Dados/Interfaces/IRepositorioBimestre.cs
+++ src/SME.Sondagem.Dados/Interfaces/IRepositorioBimestre.cs
@@ -6,10 +6,5 @@ namespace SME.Sondagem.Dados.Interfaces;
 
 public interface IRepositorioBimestre : IRepositorioBase<Bimestre>
 {
-    //Task<IEnumerable<Bimestre>> ObterTodosAsync(CancellationToken cancellationToken = default);
-    //Task<Bimestre?> ObterPorIdAsync(long id, CancellationToken cancellationToken = default);
-    //Task<long> CriarAsync(Bimestre bimestre, CancellationToken cancellationToken = default);
-    //Task<bool> AtualizarAsync(Bimestre bimestre, CancellationToken cancellationToken = default);
-    //Task<bool> ExcluirAsync(long id, CancellationToken cancellationToken = default);
     Task<ICollection<SondagemPeriodoBimestre>> ObterBimestresPorQuestionarioIdAsync(int questionarioId, CancellationToken cancellationToken = default);
 }
--- src/SME.Sondagem.Dados/Repositorio/Postgres/RepositorioBimestre.cs
+++ src/SME.Sondagem.Dados/Repositorio/Postgres/RepositorioBimestre.cs
@@ -16,57 +16,6 @@ public class RepositorioBimestre : RepositorioBase<Bimestre>, IRepositorioBimest
     {
     }
 
-    //public async Task<IEnumerable<Bimestre>> ObterTodosAsync(CancellationToken cancellationToken = default)
-    //{
-    //    return await context.Bimestres
-    //        .AsNoTracking()
-    //        .Where(p => !p.Excluido)
-    //        .OrderBy(p => p.Descricao)
-    //        .ToListAsync(cancellationToken);
-    //}
-
-    //public async Task<Bimestre?> ObterPorIdAsync(long id, CancellationToken cancellationToken = default)
-    //{
-    //    return await context.Bimestres
-    //        .FirstOrDefaultAsync(p => p.Id == id && !p.Excluido, cancellationToken);
-    //}
-
-    //public async Task<long> CriarAsync(Bimestre bimestre, CancellationToken cancellationToken = default)
-    //{
-    //    await context.Bimestres.AddAsync(bimestre, cancellationToken);
-    //    await context.SaveChangesAsync(cancellationToken);
-    //    return bimestre.Id;
-    //}
-
-    //public async Task<bool> AtualizarAsync(Bimestre bimestre, CancellationToken cancellationToken = default)
-    //{
-    //    var bimestreExistente = await context.Bimestres
-    //        .FirstOrDefaultAsync(p => p.Id == bimestre.Id && !p.Excluido, cancellationToken);
-
-    //    if (bimestreExistente == null)
-    //        return false;
-
-    //    bimestreExistente.AlteradoEm = bimestre.AlteradoEm;
-    //    bimestreExistente.AlteradoPor = bimestre.AlteradoPor;
-    //    bimestreExistente.AlteradoRF = bimestre.AlteradoRF;
-
-    //    await context.SaveChangesAsync(cancellationToken);
-    //    return true;
-    //}
-
-    //public async Task<bool> ExcluirAsync(long id, CancellationToken cancellationToken = default)
-    //{
-    //    var bimestre = await context.Bimestres
-    //        .FirstOrDefaultAsync(p => p.Id == id && !p.Excluido, cancellationToken);
-
-    //    if (bimestre == null)
-    //        return false;
-
-    //    bimestre.Excluido = true;
-    //    await context.SaveChangesAsync(cancellationToken);
-    //    return true;
-    //}
-
     public async Task<ICollection<SondagemPeriodoBimestre>> ObterBimestresPorQuestionarioIdAsync(int questionarioId, CancellationToken cancellationToken = default)
     {
         var bimestreIds = await _context.QuestionariosBimestres
