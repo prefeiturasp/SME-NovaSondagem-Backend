16da4f8e8e4f8ceb9d2bff60465ba353ba2b9390
Thiago Fernando <thiago.fernando@msn.com>
2026-02-04 11:51:27 -0300

    Remover comentarios

--- src/SME.Sondagem.Dados/Interfaces/IRepositorioQuestao.cs
+++ src/SME.Sondagem.Dados/Interfaces/IRepositorioQuestao.cs
@@ -5,11 +5,6 @@ namespace SME.Sondagem.Dados.Interfaces;
 
 public interface IRepositorioQuestao : IRepositorioBase<Questao>
 {
-    // Task<IEnumerable<Questao>> ObterTodosAsync(CancellationToken cancellationToken = default);
-    // Task<Questao?> ObterPorIdAsync(long id, CancellationToken cancellationToken = default);
-    // Task<long> CriarAsync(Questao questao, CancellationToken cancellationToken = default);
-    // Task<bool> AtualizarAsync(Questao questao, CancellationToken cancellationToken = default);
-    // Task<bool> ExcluirAsync(long id, CancellationToken cancellationToken = default);
     Task<IEnumerable<Questao>> ObterQuestoesAtivasPorFiltroAsync(
         int modalidadeId, 
         int anoLetivo, 
