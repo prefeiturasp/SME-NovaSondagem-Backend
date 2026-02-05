ed076e636a94a98219e68a2190983b0849de20fd
Mônica Chiesa <monica.chiesa@universo.univates.br>
2026-02-05 16:24:50 -0300

    Adicionar id

--- src/SME.Sondagem.Infra/Dtos/Questionario/ParametroSondagemDto.cs
+++ src/SME.Sondagem.Infra/Dtos/Questionario/ParametroSondagemDto.cs
@@ -1,11 +1,9 @@
 ﻿using SME.Sondagem.Dominio.Enums;
-using System.Text.Json.Serialization;
 
 namespace SME.Sondagem.Infrastructure.Dtos.Questionario
 {
     public class ParametroSondagemDto
     {
-        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWriting)]
         public int Id { get; set; }
         public bool Ativo { get; set; }
         public string? Descricao { get; set; }
--- src/SME.Sondagem.Infra/Dtos/Questionario/ParametroSondagemQuestionarioDto.cs
+++ src/SME.Sondagem.Infra/Dtos/Questionario/ParametroSondagemQuestionarioDto.cs
@@ -1,10 +1,7 @@
-﻿using System.Text.Json.Serialization;
-
-namespace SME.Sondagem.Infrastructure.Dtos.Questionario
+﻿namespace SME.Sondagem.Infrastructure.Dtos.Questionario
 {
     public class ParametroSondagemQuestionarioDto 
     {
-        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWriting)]
         public int Id { get; set; }
         public int IdQuestionario { get; set; }
         public int IdParametroSondagem { get; set; }
