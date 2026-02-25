97ef14e6b7137ba5f4788cd6d3915d3004ec7cf5
Mônica Chiesa <monica.chiesa@universo.univates.br>
2026-02-25 14:06:01 -0300

    Ajuste codificação do arquivo

--- src/SME.Sondagem.Aplicacao/UseCases/Questionario/Base/QuestionarioSondagemUseCaseBase.cs
+++ src/SME.Sondagem.Aplicacao/UseCases/Questionario/Base/QuestionarioSondagemUseCaseBase.cs
@@ -117,7 +117,7 @@ public abstract class QuestionarioSondagemUseCaseBase : IQuestionarioSondagemUse
             return new QuestionarioSondagemRelatorioDto
             {
                 TituloTabelaRespostas = tituloTabelaRespostas,
-                Semestre = (turma.Semestre == 0 ? "1º" : "2º") + " semestre",
+                Semestre = (turma.Semestre == 0 ? "1Âº" : "2Âº") + " semestre",
                 Estudantes = estudantes.OrderBy(e => e.Nome).ToList(),
                 Legenda = legenda,
                 UsuarioLogado = { Nome = nomeUsuarioLogado, Rf = rfUsuarioLogado }
@@ -210,7 +210,7 @@ public abstract class QuestionarioSondagemUseCaseBase : IQuestionarioSondagemUse
     {
         var descricoesExcluidas = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
         {
-            "Sim", "Não", "Nao"
+            "Sim", "NÃ£o", "Nao"
         };
 
         var opcoesUtilizadas = respostasProcessadas.RespostasConvertidas.Values
