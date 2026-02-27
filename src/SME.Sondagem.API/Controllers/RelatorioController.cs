9db7cfad8d443947b492573473f45bd57430105f
CiceroHenriqueSantos <50219791+CiceroHenriqueSantos@users.noreply.github.com>
2026-02-27 15:25:54 -0300

    Simplificando rotas

--- src/SME.Sondagem.API/Controllers/RelatorioController.cs
+++ src/SME.Sondagem.API/Controllers/RelatorioController.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Mvc;
 using SME.Sondagem.Aplicacao.Interfaces.Questionario.Relatorio;
 using SME.Sondagem.Aplicacao.Interfaces.Questionario.Relatorio.Exportacao;
 using SME.Sondagem.Infra.Constantes.Autenticacao;
-using SME.Sondagem.Infra.Dtos;
 using SME.Sondagem.Infra.Dtos.Questionario;
 using SME.Sondagem.Infrastructure.Dtos.Questionario.Relatorio;
 
@@ -15,16 +14,12 @@ namespace SME.Sondagem.API.Controllers;
 public class RelatorioController : ControllerBase
 {
     [HttpGet("sondagem-por-turma")]
-    [ProducesResponseType(typeof(RetornoBaseDto), 500)]
-    [ProducesResponseType(typeof(QuestionarioSondagemRelatorioDto), 200)]
     public async Task<IActionResult> ObterRelatorioSondagemPorTurma([FromQuery] FiltroQuestionario filtro, [FromServices] IObterSondagemRelatorioPorTurmaUseCase obterRelatorioSondagemPorTurmaUseCase, CancellationToken cancellationToken)
     {
         return Ok(await obterRelatorioSondagemPorTurmaUseCase.ObterSondagemRelatorio(filtro, cancellationToken));
     }
 
     [HttpGet("sondagem-por-turma/exportar")]
-    [ProducesResponseType(typeof(RetornoBaseDto), 500)]
-    [ProducesResponseType(typeof(QuestionarioSondagemRelatorioDto), 200)]
     public async Task<IActionResult> ExportarRelatorioSondagemPorTurma([FromQuery] FiltroRelatorio filtro, [FromServices] IExportarSondagemRelatorioPorTurmaUseCase exportarSondagemRelatorioPorTurmaUseCase, CancellationToken cancellationToken)
     {
         await exportarSondagemRelatorioPorTurmaUseCase.ExportarSondagemRelatorio(filtro, cancellationToken);
