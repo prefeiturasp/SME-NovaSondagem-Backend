f59f4e8e2a6f105c7acb55cd66131c8dbddb49fc
Mario Junior <mariosj9@gmail.com>
2026-03-02 15:59:21 -0300

    Correção ObterSondagemAtiva no RepositorioSondagem

--- src/SME.Sondagem.Dados/Repositorio/Postgres/RepositorioSondagem.cs
+++ src/SME.Sondagem.Dados/Repositorio/Postgres/RepositorioSondagem.cs
@@ -16,14 +16,14 @@ public class RepositorioSondagem : RepositorioBase<Dominio.Entidades.Sondagem.So
     public async Task<Dominio.Entidades.Sondagem.Sondagem> ObterSondagemAtiva(CancellationToken cancellationToken = default)
     {
         var dataAtual = DateTimeExtension.HorarioBrasilia();
+        var anoAtual = dataAtual.Year;
         var sondagem = await _context.Sondagens
             .AsNoTracking()
             .Where(s =>
                 !s.Excluido &&
                 s.PeriodosBimestre.Any(p =>
                     !p.Excluido &&
-                    p.DataInicio <= dataAtual &&
-                    p.DataFim >= dataAtual))
+                    p.DataInicio.Year == anoAtual && p.DataFim.Year == anoAtual))
             .Include(s => s.PeriodosBimestre.Where(p => !p.Excluido))
             .ThenInclude(p => p.Bimestre)
             .FirstOrDefaultAsync(cancellationToken);
