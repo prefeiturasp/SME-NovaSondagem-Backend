d1f0a572f21996d450df6a3d6fcb05a02bb2e51b
Mônica Chiesa <monica.chiesa@universo.univates.br>
2026-03-10 09:20:35 -0300

    Criar testes unitários e criar construtor para o sonar parar de reclamar

--- src/SME.Sondagem.Dominio/Entidades/ModalidadeComponenteCurricular.cs
+++ src/SME.Sondagem.Dominio/Entidades/ModalidadeComponenteCurricular.cs
@@ -4,6 +4,12 @@ namespace SME.Sondagem.Dominio.Entidades;
 
 public class ModalidadeComponenteCurricular : EntidadeBase
 {
+    public ModalidadeComponenteCurricular(Modalidade modalidadeId, int componenteCurricularId)
+    {
+        ModalidadeId = modalidadeId;
+        ComponenteCurricularId = componenteCurricularId;
+    }
+
     public Modalidade ModalidadeId { get; private set; }
     public int ComponenteCurricularId { get; private set; }
     public virtual ComponenteCurricular? ComponenteCurricular { get; private set; }
---
+++ teste/SME.Sondagem.Dominio.Teste/Entidades/ModalidadeComponenteCurricularTeste.cs
@@ -0,0 +1,64 @@
+﻿using SME.Sondagem.Dominio.Entidades;
+using SME.Sondagem.Dominio.Enums;
+using Xunit;
+
+namespace SME.Sondagem.Dominio.Teste.Entidades
+{
+    public class ModalidadeComponenteCurricularTeste
+    {
+        [Fact]
+        public void Construtor_DeveAtribuirModalidadeIdCorretamente()
+        {
+            var modalidade = Modalidade.Fundamental;
+            var componenteCurricularId = 10;
+
+            var entidade = new ModalidadeComponenteCurricular(modalidade, componenteCurricularId);
+
+            Assert.Equal(modalidade, entidade.ModalidadeId);
+        }
+
+        [Fact]
+        public void Construtor_DeveAtribuirComponenteCurricularIdCorretamente()
+        {
+            var modalidade = Modalidade.EJA;
+            var componenteCurricularId = 42;
+
+            var entidade = new ModalidadeComponenteCurricular(modalidade, componenteCurricularId);
+
+            Assert.Equal(componenteCurricularId, entidade.ComponenteCurricularId);
+        }
+
+        [Fact]
+        public void Construtor_DeveInicializarComponenteCurricularComoNulo()
+        {
+            var entidade = new ModalidadeComponenteCurricular(Modalidade.EducacaoInfantil, 1);
+
+            Assert.Null(entidade.ComponenteCurricular);
+        }
+
+        [Theory]
+        [InlineData(Modalidade.EducacaoInfantil, 1)]
+        [InlineData(Modalidade.EJA, 3)]
+        [InlineData(Modalidade.CIEJA, 4)]
+        [InlineData(Modalidade.Fundamental, 5)]
+        [InlineData(Modalidade.Medio, 6)]
+        [InlineData(Modalidade.CMCT, 7)]
+        [InlineData(Modalidade.MOVA, 8)]
+        [InlineData(Modalidade.ETEC, 9)]
+        public void Construtor_DeveAceitarTodasAsModalidades(Modalidade modalidade, int componenteCurricularId)
+        {
+            var entidade = new ModalidadeComponenteCurricular(modalidade, componenteCurricularId);
+
+            Assert.Equal(modalidade, entidade.ModalidadeId);
+            Assert.Equal(componenteCurricularId, entidade.ComponenteCurricularId);
+        }
+
+        [Fact]
+        public void Entidade_DeveHerdarDeEntidadeBase()
+        {
+            var entidade = new ModalidadeComponenteCurricular(Modalidade.Fundamental, 10);
+
+            Assert.IsAssignableFrom<EntidadeBase>(entidade);
+        }
+    }
+}
