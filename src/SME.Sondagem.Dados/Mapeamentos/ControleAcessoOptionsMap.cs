72b989f86242a1c13d5ca17e392920f621a1b4c7
Thiago Fernando <thiago.fernando@msn.com>
2026-03-18 10:40:17 -0300

    Parte 2

---
+++ src/SME.Sondagem.Dados/Mapeamentos/ControleAcessoOptionsMap.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SME.Sondagem.Dominio.Entidades.Configuration;
+using System.Diagnostics.CodeAnalysis;
+
+namespace SME.Sondagem.Dados.Mapeamentos
+{
+    [ExcludeFromCodeCoverage]
+    public class ControleAcessoOptionsMap : IEntityTypeConfiguration<ControleAcessoOptions>
+    {
+        public void Configure(EntityTypeBuilder<ControleAcessoOptions> builder)
+        {
+
+            builder.ToTable("controle_acesso_options");
+
+            builder.Property(e => e.GrupoSituacao)
+                .IsRequired();
+
+            builder.Property(e => e.SistemaId)
+                .IsRequired();
+
+            builder.Property(e => e.ModuloId)
+                .IsRequired();
+
+            builder.Property(e => e.CacheDuracaoMinutos)
+                .IsRequired();
+
+            builder.HasMany(e => e.ConfiguracaoPerfis)
+                .WithOne()
+                .HasForeignKey("ControleAcessoOptionsId")
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
