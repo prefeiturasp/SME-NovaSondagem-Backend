bf7404cb8298a532f851c0a079925ed848781158
CiceroHenriqueSantos <50219791+CiceroHenriqueSantos@users.noreply.github.com>
2026-04-30 11:08:53 -0300

    Integracao

--- src/SME.Sondagem.API/Controllers/Integracao/RelatorioIntegracaoController.cs
+++ src/SME.Sondagem.API/Controllers/Integracao/RelatorioIntegracaoController.cs
@@ -6,6 +6,7 @@ using SME.Sondagem.Infra.Dtos;
 using SME.Sondagem.Infra.Dtos.Proficiencia;
 using SME.Sondagem.Infra.Dtos.Questionario;
 using SME.Sondagem.Infrastructure.Dtos.Questionario.Relatorio;
+using SME.Sondagem.Infrastructure.Dtos.Relatorio;
 
 namespace SME.Sondagem.API.Controllers;
 
@@ -46,6 +47,36 @@ public class RelatorioIntegracaoController : ControllerBase
     {
             var resultado = await useCase.ExecutarAsync(proficienciaId, cancellationToken);
             return Ok(resultado);
+    }
+
+    [HttpGet("consolidado/raca")]
+    public async Task<IActionResult> ObterRelatorioSondagemConsolidadoPorRaca([FromQuery] FiltroConsolidadoDto filtro, [FromServices] IObterSondagemRelatorioConsolidadoRacaUseCase obterSondagemRelatorioConsolidadoUseCase, CancellationToken cancellationToken)
+    {
+        return Ok(await obterSondagemRelatorioConsolidadoUseCase.ObterSondagemRelatorio(filtro, cancellationToken));
+    }
+
+    [HttpGet("consolidado/genero")]
+    public async Task<IActionResult> ObterRelatorioSondagemConsolidadoPorGenero([FromQuery] FiltroConsolidadoDto filtro, [FromServices] IObterSondagemRelatorioConsolidadoGeneroUseCase obterSondagemRelatorioConsolidadoGeneroUseCase, CancellationToken cancellationToken)
+    {
+        return Ok(await obterSondagemRelatorioConsolidadoGeneroUseCase.ObterSondagemRelatorio(filtro, cancellationToken));
+    }
+
+    [HttpGet("consolidado/raca-genero")]
+    public async Task<IActionResult> ObterRelatorioSondagemConsolidadoPorRacaGenero([FromQuery] FiltroConsolidadoDto filtro, [FromServices] IObterSondagemRelatorioConsolidadoRacaGeneroUseCase obterSondagemRelatorioConsolidadoRacaGeneroUseCase, CancellationToken cancellationToken)
+    {
+        return Ok(await obterSondagemRelatorioConsolidadoRacaGeneroUseCase.ObterSondagemRelatorio(filtro, cancellationToken));
+    }
+
+    [HttpGet("consolidado/ano")]
+    public async Task<IActionResult> ObterRelatorioSondagemConsolidadoPorAno([FromQuery] FiltroConsolidadoDto filtro, [FromServices] IObterSondagemRelatorioConsolidadoAnoUseCase obterSondagemRelatorioConsolidadoAnoUseCase, CancellationToken cancellationToken)
+    {
+        return Ok(await obterSondagemRelatorioConsolidadoAnoUseCase.ObterSondagemRelatorio(filtro, cancellationToken));
+    }
+
+    [HttpGet("consolidado/bimestre")]
+    public async Task<IActionResult> ObterRelatorioSondagemConsolidadoPorBimestre([FromQuery] FiltroConsolidadoDto filtro, [FromServices] IObterSondagemRelatorioConsolidadoBimestreUseCase obterSondagemRelatorioConsolidadoBimestreUseCase, CancellationToken cancellationToken)
+    {
+        return Ok(await obterSondagemRelatorioConsolidadoBimestreUseCase.ObterSondagemRelatorio(filtro, cancellationToken));
     }
 }
 
