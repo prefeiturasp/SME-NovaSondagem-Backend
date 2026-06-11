80aeca925e4d608961912f7f82802c173586746c
Thiago Fernando <thiago.fernando@msn.com>
2026-06-11 14:16:10 -0300

    resolvidor conflito

--- src/SME.Sondagem.Infra/Services/ServicoSGPConstants.cs
+++ src/SME.Sondagem.Infra/Services/ServicoSGPConstants.cs
@@ -10,4 +10,5 @@ public static class ServicoSgpConstants
     public const string URL_SOLICITACAO_RELATORIO = "v1/solicitacao-relatorio/obter-solicitacao-relatorio";
     public const string URL_ABRANGENCIA_COMPLETA = "v1/abrangencias/integracoes/false/login/{0}/perfis/{1}/abrangencia-completa?anoLetivo={2}&modalidade={3}";
     public const string URL_OBTER_DRES = "v1/dres/integracoes";
+    public const string URL_PLANO_AEE_TURMA_EXISTE = "v1/plano-aee/integracoes/turma/{0}/existe";
 }
